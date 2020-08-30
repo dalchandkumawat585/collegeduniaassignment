2e141b4a3f6aa1bf7afa01093cf7acd75dfb3a1e
Dalchand Kumawat <dkumawat@opentext.com>
2020-08-30 16:34:05 +0530

    Updates

---
+++ precache-manifest.1dfc104ab64783639bf3e39f7ed59921.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb3416a858f1b30663e6182c6ea7e0e",
+    "url": "/collegeduniaassignment/index.html"
+  },
+  {
+    "revision": "ad0cca48150bea9a03d3",
+    "url": "/collegeduniaassignment/static/css/main.80ebb313.chunk.css"
+  },
+  {
+    "revision": "c4fbbc31088a6c8d42cc",
+    "url": "/collegeduniaassignment/static/js/2.eac6fc33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collegeduniaassignment/static/js/2.eac6fc33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0cca48150bea9a03d3",
+    "url": "/collegeduniaassignment/static/js/main.99450456.chunk.js"
+  },
+  {
+    "revision": "1b0bdd8dc86b79019fec",
+    "url": "/collegeduniaassignment/static/js/runtime-main.5c8df2ce.js"
+  },
+  {
+    "revision": "8793ec0ee96ac6622dd5064036daac2a",
+    "url": "/collegeduniaassignment/static/media/college_01.8793ec0e.jpg"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/collegeduniaassignment/static/media/college_02.619ee97e.jpg"
+  }
+]);
